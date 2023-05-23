33d93e63e9daf02fd0667d2d106a487b13c32b1d
Quinth Anthony A. Razuman <chuckierazuman@gmail.com>
2023-05-23 17:33:18 +0800

    testing autoplay

--- static/js/custom-video.js
+++ static/js/custom-video.js
@@ -12,6 +12,7 @@ player.controlBar.addChild('Button', {
   className: 'home-button',
 });
 skipBackButton = document.getElementsByClassName('vjs-skip-backward-5')[0];
+playButton = document.getElementsByClassName('vjs-big-play-button')[0];
 homeButton = document.getElementsByClassName('home-button')[0];
 homeIconPlaceholder = homeButton.querySelector('.vjs-icon-placeholder');
 homeButton.onclick = function() {
@@ -29,22 +30,21 @@ homeButton.style = "background-color: rgba(43, 51, 63, 0.7); border: 1px solid #
 var player = videojs('vid1');
 player.playlist.autoadvance(0);
 window.onload = function(event) {
-  console.log("page is fully loaded");
-  console.log(player.play());
   player.play();
+  playButton.click();
   player.ready(function() {
       var promise = player.play();
 
       if (promise !== undefined) {
         promise.then(function() {
           // Autoplay started!
-          console.log("Autoplay started!");
           player.play();
+          playButton.click();
         }).catch(function(error) {
           // Autoplay was prevented.
-          console.log("Autoplay prevented!");
         });
       }
     });
 };
 player.play();
+playButton.click();
